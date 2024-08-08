2ec410ef1468c1ab7faed51d44b7887e162989ff
Kenny Chen <kennychen3200@gmail.com>
2024-08-08 15:59:30 -0400

    thus far can create new unit with title. will add more functionality

--- make.py
+++ make.py
@@ -5,7 +5,7 @@ import re
 class FileData:
     def __init__(self, path):
         self.path = path
-        self.dir = os.path.dirname(path)
+        self.dir = os.path.dirname(path) + "/"
         self.full_filename = os.path.basename(path)
         self.filename, self.extension = os.path.splitext(self.full_filename)
 
@@ -26,15 +26,36 @@ class Unit:
 
         number_str = str(number) if number >= 10 else "0" + str(number)
         self.file_data = FileData(
-            f"./src/unit/unit_{number_str}/"
+            f"./src/unit_{number_str}/"
             f"unit_{number_str}.tex"
         )
 
-    def new(self):
-        pass
+    @staticmethod
+    def new(name):
+        unit = Unit(Unit.current().number + 1)
 
-    def current(self):
-        pass
+        os.mkdir(unit.file_data.dir)
+        with open(Unit.template_file_data.path, "r") as template_file, \
+             open(unit.file_data.path, "a") as file:
+            file_text = template_file.read().rstrip()
+            token_replacements = [
+                ["\\title{Unit}", f"\\title{{{name}}}"]
+            ]
+            for token, replacement in token_replacements:
+                file_text = file_text.replace(token, replacement)
+            file.write(file_text)
+
+    @staticmethod
+    def current():
+        dirs = [x[0] for x in os.walk("./src/")]
+        unit = 0
+
+        for dir in dirs:
+            if "unit_" in dir:
+                if (x := int(dir.split("unit_")[1])) > unit:
+                    unit = x
+
+        return Unit(unit)
 
 
 class Lesson:
@@ -49,15 +70,29 @@ class Lesson:
             f"lesson_{number_str}.tex"
         )
 
-    def new(self):
+    @staticmethod
+    def new():
+        Lesson.template_file_data.print("Lesson Template")
+
+    @staticmethod
+    def current():
         pass
 
-    def current(self):
+
+def main():
+    command = input("Commands\n"
+                    "(n) New Template\n"
+                    "(0) New Unit\n"
+                    "(1) New Lesson\n")
+
+    if command == "n":
         pass
+    elif command == "0":
+        name = input("Unit Name: ")
+        Unit.new(name)
+    elif command == "1":
+        Lesson.new()
 
 
-unit_template = FileData("./src/.unit/unit.tex")
-lesson_template = FileData("./src/.unit/lesson.tex")
-
-unit_template.print("unit_template")
-lesson_template.print("lesson_template")
+if __name__ == "__main__":
+    main()
